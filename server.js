7686992555f08083d564846140360258e3dc9af8
Alessio Pragliola <seth.pro@gmail.com>
2016-07-19 10:33:19 +0000

    final stage

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+app.set("views", __dirname + '/views');
+app.set("view engine", "pug");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render("index");
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:data', function(req,res) {
+    var query = parseInt(req.params.data,10);
+    var d;
+    
+    if(isNaN(query)) {
+        d = new Date(req.params.data);
+    }
+    else
+        d = new Date(Number(req.params.data)*1000);
+    
+    if(d.toString() != 'Invalid Date') {
+        res.end(JSON.stringify({"unix" : d.getTime()/1000, "natural" : monthNames[d.getMonth()]+' '+d.getDate()+', '+d.getFullYear()}));
+    }
+    else {
+        res.end(JSON.stringify({"unix" : null, "natural": null}));
+    }
 });
+
+app.listen(8080);
